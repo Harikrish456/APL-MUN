43a64344ad8aebe9021eae08653d2c3d53108ba4
Harikrish456 <64851856+Harikrish456@users.noreply.github.com>
2023-04-25 20:58:41 +0530

    Add files via upload

--- src/components/navbar/footer/Footer.js
+++ src/components/navbar/footer/Footer.js
@@ -7,11 +7,10 @@ const Footer = () => {
   return (
     <div className={styles.footer_basic}>
         <footer>
-              <div className={styles.social}><a href="https://www.facebook.com/APL-Global-School-155685594531951/"><FaFacebookF className={styles.icon_ion_social_instagram}/></a><a href="#"><FaInstagram className={styles.icon_ion_social_snapchat}/></a></div>
-              <ol class="list-inline">
+              <div className={styles.social}><a href="https://www.facebook.com/APL-Global-School-155685594531951/"><FaFacebookF/></a><a href="#"><FaInstagram /></a></div>
+              <ul class="list-inline">
                   <li className={styles.list_inline_item}><a href="#"></a></li>
-                  <li className={styles.list_inline_item}><a href="http://aplglobalschool.com/" target='_blank'>Website</a></li>
-              </ol>
+              </ul>
               <p className={styles.copyright} >Company Name © 2018</p>
           </footer>
       </div>
